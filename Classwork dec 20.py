ff29ac853edd39a7668f8c9da7126a0f6e3403cc
Henry <Pvrpl3Afr0@users.noreply.github.com>
2016-12-20 14:25:05 -0500

    Uploaded Python Excercise

---
+++ Classwork dec 20.py
@@ -0,0 +1,45 @@
+#Excerciese 1
+
+Year1 = 100*1.1
+print(Year1)
+
+Year2= Year1*1.1
+print(Year2)
+
+Year3= Year2*1.1
+print(Year3)
+
+Year4= Year3*1.1
+print(Year4)
+
+Year5= Year4*1.1
+print(Year5)
+
+Year6= Year5*1.1
+print(Year6)
+
+Year7= Year6*1.1
+print(Year7)
+Final= Year7*1.1
+print(Final)
+
+
+#Excercise 2
+
+savings= 100
+factor= 1.10
+print(savings*factor**8)
+
+
+#Excercise 3
+
+desc= "compund interest"
+profitable= True
+
+#Excercise 4
+
+print(savings, factor, desc, profitable)
+
+#Excercise 5
+
+print('I started with $100 and now I have $214. Great!')
