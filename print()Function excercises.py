5850cd35ca98516e2f2f04d824ad75d55d82e02a
Henry <Pvrpl3Afr0@users.noreply.github.com>
2016-12-22 14:55:17 -0500

    Add files via upload

---
+++ print()Function excercises.py
@@ -0,0 +1,49 @@
+#Question 1; What does this program print? Pay close attention to spaces.
+#print("Hello","World","!")
+
+print("Hello", "World", "!")
+
+'''
+Because of the spaces this program prints Hello World ! Instead of HelloWorld!
+or Hello World!
+'''
+
+#Question 2: What is the compile-time error in this program?
+#print("Hello", "World!)
+
+print("Hello", "World!)
+#This code won't work because the quote next to ! is missing.
+'''
+Question 3: Write three versions of the hello.py program that
+have different compile-time errors.
+'''
+
+print("Hello" world!)
+print(Hello world!)
+print("hello," "world)
+
+print("helo", "world!")
+
+#Question 4: How do you discover compile-time errors? How do you discover
+#run-time errors
+'''
+Compile time errors are essentially syntax errors, IDLE will tell you when it
+encounters an unexpected character
+
+Run time errors are logistical errors and the code won't always be able to see
+them, to discover them its helpful to have pair programming
+or troubleshooting your own code
+'''
+#Question 5
+print("Hola!")
+
+#Question 6
+print(1+2+3+4+5+6+7+8+9+10)
+
+#Question 7
+print(1*2*3*4*5*6*7*8*9*10)
+
+#Question 8
+print('-------'"\n|Henry|\n" '-------')
+
+      
